bcf3ade9836eafe9c7b1be57342066c7f01a0457
Kanika Singhal <kanikasin216@gmail.com>
2025-08-11 15:54:39 +0530

    modified section controller

--- backend/models/Section.js
+++ backend/models/Section.js
@@ -4,10 +4,14 @@ const sectionSchema = new mongoose.Schema({
   tripId: { type: mongoose.Schema.Types.ObjectId, ref: 'Trip', required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
-  activities: [{
-    activityId: { type: mongoose.Schema.Types.ObjectId, ref: 'Activity' },
-  }],
-  budget: Number
+  activities: [
+    {
+      activityId: { type: mongoose.Schema.Types.ObjectId, ref: 'Activity' },
+    }
+  ],
+  budget: { type: Number },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true }
 });
 
 export default mongoose.model("Section", sectionSchema);
