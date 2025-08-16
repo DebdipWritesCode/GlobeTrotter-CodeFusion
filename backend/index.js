1e7c6bfc603555e97d991e973859b905e68be185
Debdip Mukherjee <151666336+DebdipWritesCode@users.noreply.github.com>
2025-08-16 22:37:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,8 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://globe-trotter-code-fusion-rb2s7f4s3-nishantharkuts-projects.vercel.app"
+      "https://globe-trotter-code-fusion-rb2s7f4s3-nishantharkuts-projects.vercel.app",
+      "https://globe-trotter-code-fusion.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
